73f2a7dc0a605ac5df151ad5e572b42ca388a397
Ashton <ashtondsouza192@gmail.com>
2025-11-28 21:05:22 +0530

    Init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen text-5xl font-extrabold text-red-600">
+      App
+    </div>
+  );
+}
+
+export default App;
