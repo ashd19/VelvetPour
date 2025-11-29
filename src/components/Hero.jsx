b24234b04bd7ca2f517f9a190360a913850c895b
Ashton <ashtondsouza192@gmail.com>
2025-11-29 16:38:25 +0530

    subtitles animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,13 +5,22 @@ import { gsap } from "gsap";
 function Hero() {
   useGSAP(() => {
     const heroSplit = new SplitText(".title", { type: "chars, words" });
-    const subtitleSplit = new SplitText(".title", { type: "lines " });
+    const subtitleSplit = new SplitText(".subtitle", { type: "lines " });
     heroSplit.chars.forEach((char) => char.classList.add("text-gradient"));
     gsap.from(heroSplit.chars, {
       yPercent: 100,
       duration: 2,
       ease: "expo.out",
       stagger: 0.08,
+    });
+
+    gsap.from(subtitleSplit.lines, {
+      yPercent: 100,
+      duration: 2,
+      ease: "expo.out",
+      stagger: 0.08,
+      opacity: 0,
+      delay: 1,
     });
   }, []);
 
@@ -32,7 +41,7 @@ function Hero() {
           className="absolute right-0  md:bottom-0 top-1/2 md:w-fit w-24"
         />
         <div className="body">
-          <div className="flex  w-full gap-10 justify-between items-center mx-auto ">
+          <div className="flex px-10 w-full gap-10 justify-between items-center mx-auto ">
             <div className="space-y-5  hidden md:block">
               <p className="text-left">Cool. Crisp. Classic.</p>
               <p className="subtitle text-left text-yellow text-5xl font-modern-negra">
@@ -46,7 +55,7 @@ function Hero() {
                 creative , flair , and timeless recipes <br />- designed to
                 delight your senses.
               </p>
-              <a href="#cocktails" className="font-bold">
+              <a href="#cocktails" className="mt-10 font-bold">
                 View Cocktails
               </a>
             </div>
